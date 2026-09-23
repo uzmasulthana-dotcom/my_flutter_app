6e9dcb969b66b401cf74fc0c1dfac2440267c896
uzmasulthana-dotcom <uzmasulthana@msrit.edu>
2026-09-23 08:48:26 +0000

    Create initial Flutter application

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My First Flutter App'),
+        ),
+        body: const Center(
+          child: Text(
+            'Hello Flutter!',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
